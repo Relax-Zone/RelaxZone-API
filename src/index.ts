189a48bdb45976e6240f50c40249c6c5f3c55a22
suwizx <suwijak.pak@gmail.com>
2025-09-04 12:50:18 +0700

    add utf8 support

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,9 @@ export default {
     ctx: Context,
     
   ): Promise<Response> {
-
-    return await app.fetch(request)
+    const response = await app.fetch(request)
+    if (!response.headers.has("Content-Type"))
+      response.headers.set("Content-Type", "text/plain; charset=utf-8")
+    return response
   },
 }
