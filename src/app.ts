8c658507095fad8a10e2c14e41f9396bafb4d649
suwizx <suwijak.pak@gmail.com>
2025-09-04 11:37:14 +0700

    innit

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import Elysia from "elysia";
+
+
+export const app = new Elysia({ aot: false })
+.onError(({ code, error }) => {
+    console.log(code)
+    return new Response(JSON.stringify({ error: error.toString() ?? code }), { status: 500 })
+})
+
+
+app.get('/', () => "Hello from Elysia 🦊")
